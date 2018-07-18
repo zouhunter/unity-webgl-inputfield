4e0e9082dc1f26ead82295cd6ad1e7a5cf2dc74a
zouhangte <zouhantezbm@126.com>
2018-07-18 18:39:13 +0800

    Init

---
+++ Plugins/WebGLInputField.jslib
@@ -0,0 +1,66 @@
+﻿var WebGLInputField = {
+  SetupOverlayDialogHtml:function(defaultValue,x,y,w,h){
+      try {
+          defaultValue = Pointer_stringify(defaultValue);
+      } catch (e) {
+		  alert(e);
+		  return;
+      }
+	  var x0 = x;
+	  var y0 = y;
+	  var ucanvas = document.getElementById("gameContainer");
+	  
+	  window.onresize= function(){
+		   var offsetx = (document.body.clientWidth - ucanvas.clientWidth) * 0.5;
+		   x = x0 + offsetx;
+		   var offsety = (document.body.clientHeight - ucanvas.clientHeight) * 0.5- 42;
+	       y = y0  + offsety;
+		   $('#nativeInputDialogInput').css({left: x + 'px', top: y + 'px', width: w, height: h});
+      };
+
+	  
+	  x = x0+ (document.body.clientWidth -ucanvas.clientWidth) * 0.5;
+	  y = y0+ (document.body.clientHeight - ucanvas.clientHeight) * 0.5- 42;
+ 
+      if(!document.getElementById("nativeInputDialog")) {
+          // setup html
+          var html = '<div id="nativeInputDialog" style="background:transparent; width:100%; height:100%; margin: 0; padding: 0; position: absolute; left: 0; top:0; z-index:888;">' +
+              '<input id="nativeInputDialogInput" type="text" style="border: none; background: none; color: white; outline: none; display: block; position: relative; font-size: 20px; ">' +
+              '</div>';
+          $(document.body).append(html);
+          $('#nativeInputDialogInput').keypress(function (event) {
+              if (event.keyCode == 13) {
+                  $('#nativeInputDialog').hide();
+              }
+          });
+          $('#nativeInputDialogInput').click(function () {
+              return false;
+          });
+ 
+          $('#nativeInputDialog').click(function () {
+              $('#nativeInputDialog').hide();
+          });
+      }
+ 
+      $('#nativeInputDialogInput').val(defaultValue);
+      $('#nativeInputDialogInput').css({left: x + 'px', top: y + 'px', width: w, height: h});
+ 
+      $('#nativeInputDialog').show();
+      $('#nativeInputDialogInput').focus();
+  },
+  
+  IsOverlayDialogHtmlActive:function(){
+      return $('#nativeInputDialog').is(':visible');
+  },
+  IsOverlayDialogHtmlCanceled:function(){
+      return ($('#nativeInputDialog').is(':visible'));
+  },
+  GetOverlayHtmlInputFieldValue:function(){
+    var returnStr = $('#nativeInputDialogInput').val();
+    var bufferSize = lengthBytesUTF8(returnStr) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(returnStr, buffer, bufferSize);
+    return buffer;
+  }
+};
+mergeInto( LibraryManager.library , WebGLInputField );
---
+++ WebGLInputField.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Runtime.InteropServices;
+
+public class WebGLInputField : InputField
+{
+    private Vector3[] conner = new Vector3[4];
+    private string placeholdText;
+    private string lastText;
+    private Text placeHold
+    {
+        get
+        {
+            return placeholder.GetComponent<Text>();
+        }
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+        placeholdText = placeHold.text;
+    }
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void SetupOverlayDialogHtml(string text, int x, int y, int width, int height);
+    [DllImport("__Internal")]
+    private static extern bool IsOverlayDialogHtmlActive();
+    [DllImport("__Internal")]
+    private static extern bool IsOverlayDialogHtmlCanceled();
+    [DllImport("__Internal")]
+    private static extern string GetOverlayHtmlInputFieldValue();
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+         base.OnPointerDown(eventData);
+        var rect = GetScreenRect();
+        Debug.Log(rect);
+        SetupOverlayDialogHtml(this.text, (int)rect.x, (int)rect.y, (int)rect.width, (int)rect.height);
+        lastText = text;
+        this.text = "";
+        placeHold.text = "";
+        StartCoroutine(this.OverlayHtmlCoroutine());
+    }
+
+    private IEnumerator OverlayHtmlCoroutine()
+    {
+        caretPosition = text.Length;
+        yield return new WaitForEndOfFrame();
+        WebGLInput.captureAllKeyboardInput = false;
+		while (IsOverlayDialogHtmlActive())
+		{
+			yield return null;
+            var textFromHtml = GetOverlayHtmlInputFieldValue();
+            if(textFromHtml != this.text)
+            {
+                this.text = textFromHtml;
+                caretPosition = textFromHtml.Length;
+            }
+        }
+
+		WebGLInput.captureAllKeyboardInput = true;
+
+		if (!IsOverlayDialogHtmlCanceled())
+		{
+			this.text = GetOverlayHtmlInputFieldValue();
+           
+        }
+        else
+        {
+            this.text = lastText;
+        }
+        placeHold.text = placeholdText;
+}
+#else
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        var rect = GetScreenRect();
+        Debug.Log(rect);
+        Debug.Log(Input.mousePosition);
+    }
+
+#endif
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            this.DeactivateInputField();
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+    private Rect GetScreenRect()
+    {
+        var rectTransform = textComponent.transform as RectTransform;
+        rectTransform.GetWorldCorners(conner);
+        Debug.Log(Screen.width);
+        return new Rect(conner[0].x,Screen.height - conner[0].y, conner[3].x - conner[0].x, conner[1].y - conner[0].y);
+    }
+
+}
